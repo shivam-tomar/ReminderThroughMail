66b2fdc555d2078eee972de583411bd1615befd4
Shivam Tomar <shivame777@gmail.com>
2020-06-26 12:07:48 +0530

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<title>Infosolution</title>
+<link href="infosolution.css" rel="stylesheet">
+</head>
+<body>
+<!-- Company Name -->
+<h1 style="text-align:center; color:#4caf50;"><font face="Comic sans MS">Infosolutions Admin Page</font></h1>
+<h4 style="text-align:center; color:navy;">Please click at "Send Mails" button below by entering today's date to send the mails to customers.</h4>
+</br>
+<div class="sform">
+<form action="action.php" method="POST">
+<div class="ctext">
+<input type="text" name="tdate" placeholder="Eg: 25-Jun-2020" required>
+</div>
+<button type="submit" class="btn" name="send">Send Mails</button>
+</form>
+</div>
+</body>
+</html>
---
+++ action.php
@@ -0,0 +1,50 @@
+<?php
+// Action page of Infosolution 
+if(isset($_POST["send"])){
+	$tdate = $_POST['tdate'];
+}
+if(!empty($tdate))
+{
+	$host = 'localhost';
+	$username = 'root';
+	$password = '';
+	$dbname = 'infosolutions';
+
+	$conn = new mysqli($host,$username,$password,$dbname);
+	
+	if(mysqli_connect_errno()){
+		printf("Connect failed: %s\n", mysqli_connect_error());
+		exit();
+	}
+	else{
+		require 'vendor/autoload.php';
+		
+		$sql = "SELECT mailid, Name, LoanAmount, Duedate FROM loandata WHERE DueDate >= '$tdate' AND Status = 'Due'";
+		if ($result = mysqli_query($conn, $sql)) {
+			while ($row = mysqli_fetch_row($result)) {
+				# row[0] is the mailid at which we have to send the mail
+				$email = new \SendGrid\Mail\Mail(); 
+				$email->setFrom("shivame777@gmail.com", "Admin");
+				$email->setSubject("Reminder for your Loan Emi");
+				$email->addTo("$row[0]", "Customer");
+				$email->addContent("text/plain", "Dear customer $row[1] Please pay your loan Emi of $row[2] INR, Due date: $row[3]");
+				$sendgrid = new \SendGrid(getenv('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'));
+				try {
+					$response = $sendgrid->send($email);
+					echo "Mail Sent to $row[1]";
+				} catch (Exception $e) {
+					echo 'Caught exception: '. $e->getMessage() ."\n";
+				} 
+			}
+			mysqli_free_result($result);
+		}
+		else 
+			echo "Zero Coustomer's emi is Due";
+	}
+}
+else{
+	echo "All required";
+	die();
+}
+$conn->close();
+?>
